1e8d9aec746e86c74eb53e7790ae83b334f164a3
Nathan Burgers <nateburgers@gmail.com>
2016-10-02 23:42:35 -0400

    fixup! fixup! More intermediate work

--- src/applications/m_lrkbld/m_lrkbld_filesystem.h
+++ src/applications/m_lrkbld/m_lrkbld_filesystem.h
@@ -2,8 +2,26 @@
 #ifndef INCLUDED_M_LRKBLD_FILESYSTEM
 #define INCLUDED_M_LRKBLD_FILESYSTEM
 
+// LARK HEADERS
 #ifndef INCLUDED_M_LRKBLD_CURSOR
 #include <m_lrkbld_cursor.h>
+#endif
+
+// STANDARD LIBRARY HEADERS
+#ifndef INCLUDED_STRING
+#include <string>
+#define INCLUDED_STRING
+#endif
+
+// POSIX HEADERS
+#ifndef INCLUDED_DIRENT
+#include <dirent.h>
+#define INCLUDED_DIRENT
+#endif
+
+#ifndef INCLUDED_SYS_TYPES
+#include <sys/types.h>
+#define INCLDUED_SYS_TYPES
 #endif
 
 namespace LarkCompiler {
@@ -12,6 +30,44 @@ namespace m_lrkbld {
 using ErrorCode = int;
 
 class DirectoryEntry {
+    // This value-semantic class..
+
+  public:
+    // TYPES
+    using IdType = ino_t;
+
+  private:
+    // DATA
+    std::string d_name;
+    IdType      d_id;
+
+  public:
+    // CREATORS
+    DirectoryEntry() = default;
+
+    DirectoryEntry(const std::string&  name,
+                   IdType              id);
+
+    DirectoryEntry(std::string&& name,
+                   IdType        id);
+
+    DirectoryEntry(const DirectoryEntry&  source) = default;
+    DirectoryEntry(      DirectoryEntry&& source) = default;
+
+   ~DirectoryEntry() = default;
+    
+    // MANIPULATORS
+    DirectoryEntry& operator=(const DirectoryEntry&  source) = default;
+    DirectoryEntry& operator=(      DirectoryEntry&& source) = default;
+
+    std::string& name();
+
+    IdType& id();
+    
+    // ACCESSORS
+    const std::string& name() const;
+
+    const IdType& id() const;
 };
 
 class VirtualFileSystem {
@@ -20,9 +76,6 @@ class VirtualFileSystem {
   public:
     // CREATORS
     virtual ~VirtualFileSystem() = default;
-
-    // MANIPULATORS
-    virtual ErrorCode makeDirectory(const std::string& path) = 0;
 
     // ACCESSORS
     virtual ErrorCode getEntries(
@@ -56,7 +109,100 @@ struct FileSystemUtil {
     static ErrorCode getNativeFileSystem(FileSystem *output);
 };
 
+                        // ==========================
+                        // class DirectoryEntryCursor
+                        // ==========================
+
+class DirectoryEntryCursor : public VirtualCursor<DirectoryEntry> { 
+    // This mechanism..
+    
+  public:
+    // TYPES
+
+  private:
+    // DATA
+    DIR    *d_stream_p;
+    dirent *d_entry_p;
+
+  public:
+    // CREATORS
+    DirectoryEntryCursor() = delete;
+
+    DirectoryEntryCursor(DIR *stream);
+
+    DirectoryEntryCursor(const DirectoryEntryCursor&  source) = delete;
+    DirectoryEntryCursor(      DirectoryEntryCursor&& source) = default;
+
+    virtual ~DirectoryEntryCursor();
+
+    // MANIPULATORS
+    DirectoryEntryCursor& operator=(
+                                  const DirectoryEntryCursor& source) = delete;
+    DirectoryEntryCursor& operator=(DirectoryEntryCursor&& source)   = default;
+
+    virtual void advance();
+
+    // ACCESSORS
+    virtual void element(DirectoryEntry *output) const;
+
+    virtual bool done() const;
+};
+
+// ============================================================================
+//                            INLINE DEFINITIONS
+// ============================================================================
+
+                        // --------------------------
+                        // class DirectoryEntryCursor
+                        // --------------------------
+                         
+// CREATORS
+DirectoryEntryCursor::DirectoryEntryCursor(DIR *stream)
+    : d_stream_p(stream)
+    , d_entry_p(readdir(stream))
+{
+}
+
+DirectoryEntryCursor::~DirectoryEntryCursor()
+{
+    closedir(d_stream_p);
+}
+
+// MANIPULATORS
+void DirectoryEntryCursor::advance()
+{
+    d_entry_p = readdir(d_stream_p);
+}
+
+// ACCESSORS
+void DirectoryEntryCursor::element(DirectoryEntry *output) const
+{
+    *output = DirectoryEntry(d_entry_p->d_name,
+                             d_entry_p->d_ino);
+}
+
+bool DirectoryEntryCursor::done() const
+{
+    return nullptr == d_entry_p;
+}
+
 } // close package namespace
 } // close product namespace
+
+// ----------------------------------------------------------------------------
+// Copyright 2016 Nathan Burgers
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at 
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------
 
 #endif // INCLUDED_M_LRKBLD_FILESYSTEM
